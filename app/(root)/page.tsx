a0e4bf3978f91ae44ca0fac2c417e0f06be8fb15
Abhaya Ghimire <abhaya2047@gmail.com>
2025-01-27 15:50:46 +0000

    Completed Dynamic routing for each details page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,22 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard,{ StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+// import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUP_QUERY } from "@/sanity/lib/queries";
 
 
  
-export default async function Home({searchParams}:{
+export default async function Home({
+  searchParams
+}:{
   searchParams: Promise<{query?: string}>
 }) {
 
   const query = (await searchParams).query;
+  const params={search:query||null}
   
-  
-  const posts = await client.fetch(STARTUP_QUERY);
+  // const posts = await client.fetch(STARTUP_QUERY);
+  const {data:posts}=await sanityFetch({query:STARTUP_QUERY,params});
   // console.log(posts);
 
 
@@ -53,6 +57,7 @@ export default async function Home({searchParams}:{
       </ul>
 
     </section>
+    <SanityLive />
     </>
   );
 }
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity"
 
 export const STARTUP_QUERY =
-    defineQuery(`*[_type == "startup" && defined(slug.current)]| order(_createdAt desc){
+    defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search)|| title match $search || category match $search || author->name match $search ]| order(_createdAt desc){
         _id,
         title,
         _createdAt,
@@ -15,4 +15,24 @@ export const STARTUP_QUERY =
     }`);
 
 
-    
+export const STARTUP_BY_ID_QUERY=
+defineQuery(`*[_type == "startup" && _id==$id][0]{
+        _id,
+        title,
+        _createdAt,
+        author->{
+            _id,name,image,bio,username
+        },
+        views,
+        description,
+        category,
+        image,
+        pitch
+    }`)
+
+export const STARTUP_VIEW_QUERY=
+    defineQuery(`
+        *[_type=="startup" && _id==$id][0]{
+        _id,views
+        }
+        `)
